b71476fa446eff9c6991f2a7dbbf3fcea83b1f35
Maera <baeitsmae@gmail.com>
2022-12-19 19:31:06 +0100

    Header Pages done

---
+++ components/HeaderPages.js
@@ -0,0 +1,36 @@
+const dashboard = document.querySelector(".dashboard");
+const headerDashboard = document.createElement("div");
+const arrows = document.createElement("div");
+const arrowLeft = document.createElement("img");
+const arrowRight = document.createElement("img");
+const profileAccount = document.createElement("div");
+const userIcon = document.createElement("img");
+const userBox = document.createElement("div");
+const userName = document.createElement("span");
+const arrowDown = document.createElement("img");
+
+arrowLeft.setAttribute("src", "../assets/icons/arrow-left.png");
+arrowLeft.setAttribute("alt", "Page précedente");
+arrowRight.setAttribute("src", "../assets/icons/arrow-right(1).png");
+arrowRight.setAttribute("alt", "Page précedente");
+arrowDown.setAttribute("src", "../assets/icons/arrow-down.png");
+arrowDown.setAttribute("alt", "Voir plus");
+userIcon.setAttribute("src", "../assets/icons/user-icon.png");
+userIcon.setAttribute("alt", "Utilisateur");
+
+headerDashboard.classList.add("dashboard__header");
+arrows.classList.add("dashboard__header__arrows");
+profileAccount.classList.add("dashboard__header__profile-account");
+userBox.classList.add("dashboard__header__profile-account__user-box");
+userIcon.classList.add("dashboard__header__profile-account__user-box__user-icon")
+userName.textContent = "User Name";
+
+dashboard.appendChild(headerDashboard);
+headerDashboard.appendChild(arrows);
+arrows.appendChild(arrowLeft);
+arrows.appendChild(arrowRight);
+headerDashboard.appendChild(profileAccount);
+profileAccount.appendChild(userIcon);
+profileAccount.appendChild(userBox);
+userBox.appendChild(userName);
+userBox.appendChild(arrowDown);
