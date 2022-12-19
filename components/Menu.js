4494c4a0797ade8638575e8f115451d97dc5d81f
Maera <baeitsmae@gmail.com>
2022-12-19 16:44:12 +0100

    Component Menu done

---
+++ components/Menu.js
@@ -0,0 +1,51 @@
+const header                = document.querySelector(".header");
+const nav                   = document.createElement("nav");
+const logoWhite             = document.createElement("div");
+const logo                  = document.createElement("img");
+const title                 = document.createElement("h1");
+const menu                  = document.createElement("ul");
+
+const arrayMenu             = [
+    { "icons": "../assets/icons/home-icon.png", "items": "Accueil" }, 
+    { "icons": "../assets/icons/search-icon.png", "items": "Rechercher" }, 
+    { "icons": "../assets/icons/bibli-icon.png", "items": "Bibliothèque" }, 
+    { "icons": "../assets/icons/add-playlist-icon.png", "items": "Créer une playlist" },  
+    { "icons": "../assets/icons/like-icon.png", "items": "Titres likés" }
+]
+
+arrayMenu.forEach((elem) => {
+    const menuItems         = document.createElement("li");
+    const linkMenu          = document.createElement("a");
+    const icons             = document.createElement("img");
+    const items             = document.createElement("span");
+
+    items.textContent       = elem.items;
+
+    icons.setAttribute("src", elem.icons);
+    icons.setAttribute("alt", "Icône" + elem.items);
+
+    menu.appendChild(menuItems);
+    menuItems.appendChild(linkMenu);
+    linkMenu.appendChild(icons);
+    linkMenu.appendChild(items);
+})
+
+
+logo.setAttribute("src", "../assets/icons/logo-white.png");
+logo.setAttribute("alt", "Logo Spotifly");
+
+nav.classList.add("header__nav");
+logoWhite.classList.add("header__nav__logoWhite");
+title.classList.add("header__nav__logoWhite__title");
+menu.classList.add("header__nav__menu");
+
+const menuItemClass = menu.children[2];
+menuItemClass.classList.add("add-border");
+
+title.textContent           = "Spotifly";
+
+header.appendChild(nav);
+nav.appendChild(logoWhite);
+logoWhite.appendChild(logo);
+logoWhite.appendChild(title);
+nav.appendChild(menu);
